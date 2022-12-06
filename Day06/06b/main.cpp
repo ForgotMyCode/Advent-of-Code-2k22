eda29c793468ced8d45f714b54fc8600de628937
ForgotMyCode <nespamujtady@gmail.com>
2022-12-06 10:23:53 +0100

    Day 06

---
+++ Day06/06b/main.cpp
@@ -0,0 +1,43 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <unordered_map>
+
+int main() {
+	std::fstream input("input.txt");
+
+	[[unlikely]]
+	if(!input.is_open()) {
+		std::cerr << "Could not open input file.\n";
+		exit(1);
+	}
+
+	std::string signal{};
+
+	[[unlikely]]
+	if(!std::getline(input, signal)) {
+		std::cerr << "Could not read the signal\n";
+		exit(2);
+	}
+
+	std::unordered_map<char, int> timeStamps{};
+
+	int blockUntil = 14;
+
+	for(int i = 0; i < signal.size(); ++i) {
+		auto& timeStamp = timeStamps[signal[i]];
+		
+		blockUntil = std::max(blockUntil, timeStamp + 14);
+
+		std::cout << "blockUntil = " << blockUntil << '\n';
+
+		timeStamp = i;
+
+		if(i >= blockUntil) {
+			std::cout << "Solution 1: " << (i + 1) << std::endl;
+			break;
+		}
+	}
+
+	input.close();
+}
